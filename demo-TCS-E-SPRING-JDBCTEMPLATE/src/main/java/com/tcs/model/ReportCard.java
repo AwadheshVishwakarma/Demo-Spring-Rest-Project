7f7f2ba39df74421da65a7272d21dcc27ffdb44d
AwadheshVishwakarma <102027132+AwadheshVishwakarma@users.noreply.github.com>
2022-03-31 21:41:54 -0700

    demo-TCS-E-SPRING-JDBCTEMPLATE

---
+++ demo-TCS-E-SPRING-JDBCTEMPLATE/src/main/java/com/tcs/model/ReportCard.java
@@ -0,0 +1,46 @@
+package com.tcs.model;
+
+
+import java.util.ArrayList;
+import java.util.Date;
+
+/**
+ * @author springuser12
+ *
+ */
+
+
+public class ReportCard {
+	/*
+	 * private long id; private String firstName; private String lastName; private
+	 * String email; private String mobile; private Date Dob; private int c1;
+	 * private int c2; private int c3; private int c4; private String
+	 * studentaApproval; private long studentPercentage; private String
+	 * StudentResult; public long getId() { return id; } public void setId(long id)
+	 * { this.id = id; } public String getFirstName() { return firstName; } public
+	 * void setFirstName(String firstName) { this.firstName = firstName; } public
+	 * String getLastName() { return lastName; } public void setLastName(String
+	 * lastName) { this.lastName = lastName; } public String getEmail() { return
+	 * email; } public void setEmail(String email) { this.email = email; }
+	 * 
+	 * public int getC1() { return c1; } public void setC1(int c1) { this.c1 = c1; }
+	 * public int getC2() { return c2; } public void setC2(int c2) { this.c2 = c2; }
+	 * public int getC3() { return c3; } public void setC3(int c3) { this.c3 = c3; }
+	 * public int getC4() { return c4; } public void setC4(int c4) { this.c4 = c4; }
+	 * public String getStudentaApproval() { return studentaApproval; } public void
+	 * setStudentaApproval(String studentaApproval) { this.studentaApproval =
+	 * studentaApproval; } public long getStudentPercentage() { return
+	 * studentPercentage; } public void setStudentPercentage(long studentPercentage)
+	 * { this.studentPercentage = studentPercentage; } public String
+	 * getStudentResult() { return StudentResult; } public void
+	 * setStudentResult(String studentResult) { StudentResult = studentResult; }
+	 * public Date getDob() { return Dob; } public void setDob(Date dob) { Dob =
+	 * dob; } public String getMobile() { return mobile; } public void
+	 * setMobile(String mobile) { this.mobile = mobile; }
+	 * 
+	 * 
+	 */}
+
+
+
+
